db985833ecb3e186610c2379be468660e7e6dbd5
zringis <zringis@gmail.com>
2019-04-29 11:59:30 -0400

    Added Restaurant Index View

---
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantRaterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterMVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext _db = new RestaurantDBContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_db.Restaurants.ToList());
+        }
+    }
+}
--- RestaurantRaterMVC/Models/Restaurant.cs
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace RestaurantRaterMVC.Models
 {
     public class Restaurant
     {
+        [Display(Name = "Restaurant ID")]
         public int RestaurantID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
