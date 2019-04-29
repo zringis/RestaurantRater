c6e9f20c93267cfbbb4f7ff6a4f72187af471413
zringis <zringis@gmail.com>
2019-04-29 16:33:28 -0400

    Added Read Controller Method and View

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -39,6 +39,21 @@ namespace RestaurantRaterMVC.Controllers
             return View(restaurant);
         }
 
+        // GET: Resturant/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
         // GET: Restaurant/Edit
         public ActionResult Edit(int? id)
         {
